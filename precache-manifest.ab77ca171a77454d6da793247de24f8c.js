70967a93814021bb1142ef7a84f29245baca915b
Rebecca Haliburton <haliburtonre@gmail.com>
2019-08-15 10:42:58 -0700

    build

--- precache-manifest.ab77ca171a77454d6da793247de24f8c.js
+++ precache-manifest.ab77ca171a77454d6da793247de24f8c.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/portfolio/public/js/chunk-vendors.3f518fe7.js"
   },
   {
-    "revision": "e8db3d9de2f31dbe57b4",
-    "url": "/portfolio/public/js/app.ab86787c.js"
+    "revision": "d4e788ef66603793b50e",
+    "url": "/portfolio/public/js/app.68d8316c.js"
   },
   {
     "revision": "c44827f2f36ed601cd6b",
     "url": "/portfolio/public/css/chunk-vendors.bbeacb79.css"
   },
   {
-    "revision": "e8db3d9de2f31dbe57b4",
-    "url": "/portfolio/public/css/app.f93dbe7e.css"
+    "revision": "d4e788ef66603793b50e",
+    "url": "/portfolio/public/css/app.42113ceb.css"
   },
   {
-    "revision": "748829da685fb6e9e9302cc2beda360b",
+    "revision": "368b6db55d08f39050adec66a9e83afe",
     "url": "/portfolio/index.html"
   }
 ];
